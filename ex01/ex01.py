e4ad70f284b519a0aff5e04c3271f0c651bfd2f2
CAD Client <bangchu12@naver.com>
2016-03-09 20:19:51 +0900

    ex01 # 연습

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,11 +1,11 @@
 print "Hello Wrold"
-#print "Hello Again"
-#print "I like typing this."
-#print "This is fun."
-#print "Yay! Printing."
-#print "I'd much rather you 'not'."
-#print 'i"said" do not touch this.'
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'i"said" do not touch this.'
 
-#print("Geon Hui")
-#print("2016 03 09 19:49")
-#print("Lee Se Dol : Alpha Go = 0:1")
+print("Geon Hui")
+print("2016 03 09 19:49")
+print("Lee Se Dol : Alpha Go = 0:1")
