a25ff21da1e8f2e08c4f39ff60d57b144537c676
CAD Client <CAD Client>
2016-04-13 18:28:03 +0900

    raw_input ()에서 ()안에 들어가는 것은 컴퓨터와 대화할때 맨 앞줄에 나오는 기호다 .

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -8,7 +8,7 @@ print "Here's your file %r:" % filename
 print txt.read()
 
 print "Type the filename again:"
-file_again = raw_input("> ")
+file_again = raw_input("- ")
 
 txt_again = open(file_again)
 
