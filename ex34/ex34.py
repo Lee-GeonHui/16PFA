4ab9bcbbd4ac1ab58897aff5dc232aa71f1a8471
CAD Client <CAD Client>
2016-05-18 19:33:56 +0900

    수정

--- ex34/ex34.py
+++ ex34/ex34.py
@@ -1,2 +1,20 @@
+# -*-coding:utf8
+
+
+
 animal = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 print animal[0]
+#리스트 설정하는 법
+#리스트는 0 이 첫번째 리스트를 뜻한다.
+
+a = [1, 2, 3]
+a[2] = 4
+print a[0:3]
+#리시트를 수정하고 싶다면 리스트 아래에 항목을 지정후 바꾸고 싶은 값으로 정한다.
+
+a = [1, 2, 3, 4, 5]
+a[1:3] = ['a', 'b', 'c']
+print a
+#연속된 리스트를 수정할 수 있게함
+
+
