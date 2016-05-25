cde7a9905bc0173a968573cc6a18447fbff48cd2
P.sangmin <orbird@naver.com>
2016-05-25 17:50:23 +0900

    파보나치 수열

--- math/math1.py
+++ math/math1.py
@@ -4,11 +4,32 @@
 #짝수이면서 4백만 이하인 모든 항을 더하면 얼마가 됩니까?
 #대신짝수이면서 4백만 이하인 가장 큰 항은 얼마가 됩니까?
 #* Recursion 재귀함수 를 이용해 볼 수 있겠습니까?
+#로봇팔같은 기계에 리커션을 쓰면 안좋다.
+import sys
 
-def factorial(n):
-    if 1 == n:
+
+def fibonacci(n):
+    if 2==n:
+        return 2
+    elif 1 ==n:
         return 1
+    elif 0 >= n:
+        return 0
     else:
-        return n * factorial(n - 1)
+        return fibonacci(n - 2) + fibonacci(n - 1)
 
-print(factorial(5))
+
+def main():
+    for i in range(1, 10 + 1):
+        print ("Fibonacci(%d) = %d" %(i, fibonacci(i)))
+
+    i = 11
+    while True:
+        f = fibonacci(i)
+        if f > 4000000:
+            print ("Fibonacci(%d) = %d" % (i, f))
+            sys.exit(0)
+        i += 1
+
+if '__main__' == __name__:
+    main()
